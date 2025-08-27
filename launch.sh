28d741cc3a24903f6885a065f0b1dc826614c4ca
Tjorkinthus <56727760+Tjorkinthus@users.noreply.github.com>
2025-08-27 21:55:46 +0200

    Dev Lancement sous Windows

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@
 echo "=== Lancement de GenerateurUML ==="
 
 # Définition des chemins
-JAVAFX_LIB_DIR="./JavaFX_Lib"
+JAVAFX_LIB_DIR="./JavaFX_Lib/Lnx/lib"
 EXT_DRIVER_DIR="./Ext_Driver"
 JAR_NAME="GenerateurUML.jar"
 MAIN_CLASS="InterfaceGenerateurUML"
